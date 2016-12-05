8b25008c8aff8e83a6c80a398fa57bbf50fb3918
mikevhe18 <viriyananda.michael@gmail.com>
2016-12-05 20:11:10 +0700

    [8.0][ADD] stock_location_accounting_info (#2)
    * Add module stock_location_type_accounting_info
    
    Changes position in xml to attributes
    
    * Add OpenSynergy Indonesia Branding

---
+++ stock_location_type_accounting_info/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# © 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Location Type Accounting Information",
+    "version": "8.0.1.0.0",
+    "summary": "Allows to define accounting information",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Stock",
+    "depends": ["stock_account"],
+    "data": ["views/stock_location_view.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+}
