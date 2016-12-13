c3c14c0775355e5fddb462e3a4559cfddbb62601
mike <viriyananda.michael@gmail.com>
2016-12-13 10:08:04 +0700

    [ADD] stock_move_note

---
+++ stock_move_note/__openerp__.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Move Notes",
+    "version": "8.0.1.0.0",
+    "summary": "Adds Stock Move Notes",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["stock"],
+    "data": ["views/stock_move_view.xml"],
+}
