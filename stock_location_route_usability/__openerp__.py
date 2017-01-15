6a6db997907eda0ecd157950128c808988aed75f
mike <viriyananda.michael@gmail.com>
2017-01-15 11:12:55 +0700

    Adding new module stock_location_route_usability

---
+++ stock_location_route_usability/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Location Route Usability",
+    "version": "8.0.1.0.0",
+    "summary": "Control usability for Routes",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["stock"],
+    "data": [
+        "views/stock_location_route_views.xml"
+    ],
+}
