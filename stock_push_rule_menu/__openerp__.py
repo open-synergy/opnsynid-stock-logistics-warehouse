2f6b6b65fa34ed6c80aac949e2451d2f0babcb28
mike <viriyananda.michael@gmail.com>
2017-01-15 09:57:03 +0700

    Revision

--- stock_push_rule_menu/__openerp__.py
+++ stock_push_rule_menu/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Push Rules Menu",
-    "version": "8.0.1.1.0",
+    "version": "8.0.1.2.0",
     "summary": "Add Push Rules Menu",
     "category": "Stock Management",
     "website": "https://opensynergy-indonesia.com",
