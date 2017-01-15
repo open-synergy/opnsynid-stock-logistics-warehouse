23119073186ca91b3ea35e5baf3c21f091a472dd
Andhitia Rama <andhitia.r@gmail.com>
2017-01-15 17:23:31 +0700

    Update __openerp__.py
    1. Change module's name
    2. Change dependency

--- stock_warehouse_interwarehouse/__openerp__.py
+++ stock_warehouse_interwarehouse/__openerp__.py
@@ -2,13 +2,13 @@
 # © 2017 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Stock Warehouse Interwarehouse",
+    "name": "Inter-Warehouse Product Movement",
     "version": "8.0.1.0.0",
-    "summary": "Stock Warehouse Extension",
+    "summary": "More control to inter-warehouse product movements",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
     "category": "Stock Management",
-    "depends": ["stock"],
+    "depends": ["stock_route_transit"],
     "data": [
         "views/stock_warehouse_view.xml"
     ],
