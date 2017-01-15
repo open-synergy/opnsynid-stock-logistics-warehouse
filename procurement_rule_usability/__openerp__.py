eda9aacdfea2373b172379a03ab19a5d362898ee
mike <viriyananda.michael@gmail.com>
2017-01-15 10:16:02 +0700

    Adding new module procurement_rule_usability

---
+++ procurement_rule_usability/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Procurement Rule Usability",
+    "version": "8.0.1.0.0",
+    "summary": "Control usability for procurement rule",
+    "category": "Procurement",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["procurement"],
+    "data": [
+        "views/procurement_rule_views.xml"
+    ],
+}
