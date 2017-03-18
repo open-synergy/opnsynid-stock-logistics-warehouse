6f513d3cffde63a32fb9e12b8a92261094e9ba6a
Andhitia Rama <andhitia.r@gmail.com>
2017-03-18 10:45:07 +0700

    [ADD][8.0] stock_move_technical_information

---
+++ stock_move_technical_information/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Move Technical Information",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock",
+        ],
+    "data": [
+        "views/stock_move_views.xml",
+        ],
+}
