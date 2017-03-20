249ed5809c2b69ab3ad0532b5a6c05166eb639b2
Andhitia Rama <andhitia.r@gmail.com>
2017-03-20 11:05:59 +0700

    [ADD][8.0] stock_move_reassign_picking

---
+++ stock_move_reassign_picking/models/stock_move.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.multi
+    def action_reassign_picking(self):
+        for move in self:
+            move._picking_assign(
+                move.group_id.id,
+                move.location_id.id,
+                move.location_dest_id.id)
