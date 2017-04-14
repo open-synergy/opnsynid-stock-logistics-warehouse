0a4b8262318fc8f4038fcdefb90a692ee3dc9a6a
Andhitia Rama <andhitia.r@gmail.com>
2017-04-14 07:33:37 +0700

    [ADD][8.0] stock_picking_product_policy

---
+++ stock_picking_product_policy/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Product Policy",
+    "version": "8.0.1.0.0",
+    "category": "Stock",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking_type_views.xml",
+        "views/stock_picking_views.xml",
+        "views/stock_move_views.xml",
+    ],
+}
---
+++ stock_picking_product_policy/models/stock_picking_type.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+# 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields, api
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    @api.depends(
+        "allowed_product_categ_ids",
+        "allowed_product_ids",
+    )
+    def _compute_all_allowed_product_ids(self):
+        obj_product = self.env["product.product"]
+        for picking_type in self:
+            products = picking_type.allowed_product_ids
+            category_ids = picking_type.allowed_product_categ_ids.ids
+            criteria = [
+                ("categ_id", "in", category_ids),
+            ]
+            products += obj_product.search(criteria)
+            picking_type.all_allowed_product_ids = products
+
+    limit_product_selection = fields.Boolean(
+        string="Limit Product Selection",
+    )
+    allowed_product_categ_ids = fields.Many2many(
+        string="Allowed Product Categories",
+        comodel_name="product.category",
+    )
+    allowed_product_ids = fields.Many2many(
+        string="Allowed Product",
+        comodel_name="product.product",
+        relation="rel_picking_type_2_product",
+        col1="type_id",
+        col2="product_id",
+    )
+    all_allowed_product_ids = fields.Many2many(
+        string="All Allowed Product",
+        comodel_name="product.product",
+        compute="_compute_all_allowed_product_ids",
+        store=True,
+        relation="rel_picking_type_2_all_product",
+        col1="type_id",
+        col2="product_id",
+    )
