663fb22d77f9145c3fc54857784e9f9a81f709df
Andhitia Rama <andhitia.r@gmail.com>
2017-05-03 15:43:56 +0700

    [ADD][8.0] stock_picking_view_layout_multi_address

---
+++ stock_picking_view_layout_multi_address/models/stock_picking_type.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    tree_show_consignee_id = fields.Boolean(
+        string="Show Consignee on Tree View",
+        default=True,
+    )
+    tree_show_delivery_address_id = fields.Boolean(
+        string="Show Delivery Address on Tree View",
+        default=True,
+    )
+    tree_show_origin_address_id = fields.Boolean(
+        string="Show Origin Address on Tree View",
+        default=True,
+    )
+    form_show_consignee_id = fields.Boolean(
+        string="Show Consignee on Form View",
+        default=True,
+    )
+    form_show_delivery_address_id = fields.Boolean(
+        string="Show Delivery Address on Form View",
+        default=True,
+    )
+    form_show_origin_address_id = fields.Boolean(
+        string="Show Origin Address on Form View",
+        default=True,
+    )
+    form_required_consignee_id = fields.Boolean(
+        string="Required Consignee on Form View",
+        default=False,
+    )
+    form_required_delivery_address_id = fields.Boolean(
+        string="Required Delivery Address on Form View",
+        default=False,
+    )
+    form_required_origin_address_id = fields.Boolean(
+        string="Required Origin Address on Form View",
+        default=False,
+    )
