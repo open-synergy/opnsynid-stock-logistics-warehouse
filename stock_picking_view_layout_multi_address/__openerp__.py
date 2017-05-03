86fe7a5380677735a2c1db4742f837fed4146623
Andhitia Rama <andhitia.r@gmail.com>
2017-05-03 15:43:56 +0700

    [ADD][8.0] stock_picking_view_layout_multi_address

---
+++ stock_picking_view_layout_multi_address/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking View Layout - Multi Address Ext",
+    "version": "8.0.1.0.0",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Stock Management",
+    "depends": [
+        "stock_transport_multi_address",
+        "stock_picking_view_layout",
+    ],
+    "data": [
+        "views/stock_picking_views.xml",
+        "views/stock_picking_type_views.xml",
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
