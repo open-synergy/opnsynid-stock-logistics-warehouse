40cf1d4c528b2f846bfbad006baf046d8a7acef2
Andhitia Rama <andhitia.r@gmail.com>
2017-05-07 13:18:29 +0700

    [ADD][8.0] stock_waste_operation

---
+++ stock_waste_operation/models/stock_warehouse.py
@@ -0,0 +1,97 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api, _
+from openerp.exceptions import Warning as UserError
+
+
+class StockWarehouse(models.Model):
+    _inherit = "stock.warehouse"
+
+    waste_type_id = fields.Many2one(
+        string="Waste Type",
+        comodel_name="stock.picking.type"
+    )
+    waste_loc_id = fields.Many2one(
+        string="Waste Location",
+        comodel_name="stock.location"
+    )
+
+    @api.multi
+    def _prepare_waste_location(self):
+        self.ensure_one()
+        parent_location = self.view_location_id
+        data = {
+            "name": _("Waste"),
+            "location_id": parent_location.id,
+            "usage": "inventory",
+            "active": True
+
+        }
+        return data
+
+    @api.multi
+    def _prepare_waste_sequence(self):
+        self.ensure_one()
+        data = {
+            "name": self.code + " - Waste",
+            "prefix": self.code + "/SCR/",
+            "padding": 6
+        }
+        return data
+
+    @api.multi
+    def _prepare_waste_type(self):
+        self.ensure_one()
+        obj_sequence = self.env['ir.sequence']
+        location = self.lot_stock_id
+        waste_loc = self._get_waste_location()
+
+        sequence = obj_sequence.create(
+            self._prepare_waste_sequence())
+
+        data = {
+            "name": _("Waste"),
+            "warehouse_id": self.id,
+            "sequence_id": sequence.id,
+            "code": "outgoing",
+            "default_location_src_id": location.id,
+            "allowed_location_ids": [(6, 0, [location.id])],
+            "default_location_dest_id": waste_loc.id,
+            "allowed_dest_location_ids": [(6, 0, [waste_loc.id])],
+        }
+        return data
+
+    @api.multi
+    def _get_waste_location(self):
+        self.ensure_one()
+        if not self.waste_loc_id:
+            raise UserError(_("No waste location"))
+        return self.waste_loc_id
+
+    @api.multi
+    def _create_waste_loc(self):
+        self.ensure_one()
+        obj_loc = self.env["stock.location"]
+        waste_loc = obj_loc.create(
+            self._prepare_waste_location())
+        self.waste_loc_id = waste_loc.id
+
+    @api.multi
+    def _create_waste_type(self):
+        self.ensure_one()
+        obj_type = self.env["stock.picking.type"]
+        waste_type = obj_type.create(
+            self._prepare_waste_type())
+        self.waste_type_id = waste_type.id
+
+    @api.multi
+    def button_create_waste_loc(self):
+        for wh in self:
+            wh._create_waste_loc()
+
+    @api.multi
+    def button_create_waste_type(self):
+        for wh in self:
+            wh._create_waste_type()
