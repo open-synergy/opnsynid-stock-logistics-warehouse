d16dc7a13140c690777a8542433f22b4b8e72cd3
Andhitia Rama <andhitia.r@gmail.com>
2017-05-08 13:29:11 +0700

    [ADD][8.0] stock_picking_type_accounting_information

---
+++ stock_picking_type_accounting_configuration/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Picking Type Accounting Configuration",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_account",
+    ],
+    "data": [
+        "views/stock_picking_type_views.xml",
+    ],
+}
