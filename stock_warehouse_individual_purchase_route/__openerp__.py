299fff8801c59201af66f0809daab1389447b855
Andhitia Rama <andhitia.r@gmail.com>
2017-05-10 21:30:27 +0700

    [ADD][8.0] stock_warehouse_individual_purchase_route

---
+++ stock_warehouse_individual_purchase_route/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Individual Buy Route for Each Warehouse",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_warehouse_technical_information",
+        "purchase",
+    ],
+    "data": [
+        "views/stock_warehouse_views.xml",
+    ],
+}
