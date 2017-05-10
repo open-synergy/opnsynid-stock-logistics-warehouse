563b862706a5c552c127621c36623e0f5f1eec22
Andhitia Rama <andhitia.r@gmail.com>
2017-05-10 21:30:27 +0700

    [ADD][8.0] stock_warehouse_individual_purchase_route

---
+++ stock_warehouse_individual_purchase_route/models/stock_warehouse.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api, fields
+
+
+class StockWarehouse(models.Model):
+    _inherit = "stock.warehouse"
+
+    buy_route_id = fields.Many2one(
+        string="Buy Route",
+        comodel_name="stock.location.route",
+    )
+
+    @api.model
+    def _get_buy_pull_rule(self, warehouse):
+        result = super(StockWarehouse, self)._get_buy_pull_rule(warehouse)
+        obj_route = self.env["stock.location.route"]
+        if warehouse.buy_route_id:
+            result["route_id"] = warehouse.buy_route_id.id
+        else:
+            route = obj_route.create(warehouse._prepare_buy_route())
+            result["route_id"] = route.id
+            warehouse.buy_route_id = route.id
+
+        return result
+
+    @api.multi
+    def _prepare_buy_route(self):
+        self.ensure_one()
+        return {
+            "name": "Buy: %s" % self.name,
+            "product_categ_selectable": True,
+            "product_selectable": False,
+            "sequence": 10,
+        }
