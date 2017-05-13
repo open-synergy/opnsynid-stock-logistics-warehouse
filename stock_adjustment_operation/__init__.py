09d0424e4f791b4e8a27add64f43887120ea840f
Andhitia Rama <andhitia.r@gmail.com>
2017-05-13 08:28:49 +0700

    [8.0.1.0.0] stock_adjustment_operation

---
+++ stock_adjustment_operation/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from .hooks import post_init_hook
+from . import models
