9d31df6a397c0a6791049c9af6dc12db316475db
Andhitia Rama <andhitia.r@gmail.com>
2017-07-13 05:07:04 +0700

    [8.0.1.0.0][stock_warehouse_manager]

---
+++ stock_warehouse_manager/models/stock_warehouse.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockWarehouse(models.Model):
+    _inherit = "stock.warehouse"
+
+    manager_id = fields.Many2one(
+        string="Manager",
+        comodel_name="res.users",
+    )
