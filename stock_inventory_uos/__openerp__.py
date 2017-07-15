45340458d3d2ad644429f4a0c7995d34a96e6c07
Andhitia Rama <andhitia.r@gmail.com>
2017-07-15 20:16:17 +0700

    [8.0.1.0.0][stock_inventory_uos]

---
+++ stock_inventory_uos/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Inventory Adjustment UoS Information",
+    "version": "8.0.1.0.4",
+    "author": "OpenSynergy Indonesia",
+    "website": "https://opensynergy-indonesia.com",
+    "category": "Stock Management",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_inventory_views.xml"
+    ],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ stock_inventory_uos/models/stock_inventory.py
@@ -0,0 +1,61 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class StockInventoryLine(models.Model):
+    _inherit = "stock.inventory.line"
+
+    @api.depends(
+        "product_id",
+        "product_uom_id",
+        "product_qty",
+        "theoretical_qty",
+    )
+    @api.multi
+    def _compute_uos(self):
+        for line in self:
+            line.product_uos_qty = line.product_uos_theoretical_qty = 0.0
+            line_uom = line.product_uom_id
+            product_uom = line.product_id.uom_id
+            product_qty = self.env["product.uom"]._compute_qty(
+                line_uom.id, line.product_qty, product_uom.id)
+            product_theoretical_qty = self.env["product.uom"]._compute_qty(
+                line_uom.id, line.theoretical_qty, product_uom.id)
+            if line.product_id.uos_id:
+                line.product_uos_id = line.product_id.uos_id.id
+                line.product_uos_qty = product_qty * line.product_id.uos_coeff
+                line.product_uos_theoretical_qty = product_theoretical_qty * \
+                    line.product_id.uos_coeff
+
+    product_uos_id = fields.Many2one(
+        string="UoS",
+        comodel_name="product.uom",
+        store=True,
+        compute="_compute_uos",
+    )
+    product_uos_qty = fields.Float(
+        string="UoS Qty.",
+        store=True,
+        compute="_compute_uos",
+    )
+    product_uos_theoretical_qty = fields.Float(
+        string="UoS Theoretical Qty.",
+        store=True,
+        compute="_compute_uos",
+    )
+
+    @api.model
+    def _resolve_inventory_line(self, inventory_line):
+        move_id = super(StockInventoryLine, self).\
+            _resolve_inventory_line(inventory_line)
+        if inventory_line.product_uos_id:
+            move = self.env["stock.move"].browse(
+                [move_id])[0]
+            move.write({
+                "product_uos": inventory_line.product_uos_id.id,
+                "product_uos_qty": inventory_line.product_uos_qty,
+            })
+        return move_id
