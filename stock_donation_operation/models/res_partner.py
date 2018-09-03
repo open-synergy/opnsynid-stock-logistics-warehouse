97f1d521d5f882bcc83895a1cca593df61bb9ca6
mikevhe18 <viriyananda.michael@gmail.com>
2018-09-03 16:00:58 +0700

    [8.0.1.0.0][stock_donation_operation]

---
+++ stock_donation_operation/models/res_partner.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    property_stock_donation_in_id = fields.Many2one(
+        string="Donation In Location",
+        comodel_name="stock.location",
+        company_dependent=True,
+    )
+    property_stock_donation_out_id = fields.Many2one(
+        string="Donation Out Location",
+        comodel_name="stock.location",
+        company_dependent=True,
+    )
