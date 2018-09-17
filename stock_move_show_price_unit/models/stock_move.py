502073b82d25c60dbfe6ad025f55d217708c37c6
Andhitia Rama <andhitia.r@gmail.com>
2018-09-18 01:45:06 +0700

    [8.0.1.0.0] stock_move_show_price_unit

---
+++ stock_move_show_price_unit/models/stock_move.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api, fields
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    @api.depends(
+        "picking_type_id"
+    )
+    @api.multi
+    def _compute_show_price_unit(self):
+        for move in self:
+            move.show_price_unit = False
+            if move.picking_type_id and move.picking_type_id.show_price_unit:
+                move.show_price_unit = True
+
+    show_price_unit = fields.Boolean(
+        string="Show Price Unit",
+        compute="_compute_show_price_unit",
+        store=False,
+    )
