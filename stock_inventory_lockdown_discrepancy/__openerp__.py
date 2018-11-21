b9a9fd5076d243d40dfa1b8ed105ca1fbd2fdbfc
Andhitia Rama <andhitia.r@gmail.com>
2018-11-21 12:44:19 +0700

    [8.0.1.0.1] stock_inventory_lockdown_discrepancy

--- stock_inventory_lockdown_discrepancy/__openerp__.py
+++ stock_inventory_lockdown_discrepancy/__openerp__.py
@@ -4,7 +4,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Integration Between Inventory Lockdown and Discrepancy",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.0.1",
     "category": "Stock Management",
     "website": "https://opensynergy-indonesia.com",
     "author": "OpenSynergy Indonesia",
