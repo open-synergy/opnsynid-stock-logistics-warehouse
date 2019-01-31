e29f1f7ff9c5450831df42eb495c8dfac020774e
Andhitia Rama <andhitia.r@gmail.com>
2019-01-31 17:47:30 +0700

    [8.0.1.0.0] stock_operation_subtype

---
+++ stock_operation_subtype/hooks.py
@@ -0,0 +1,83 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import SUPERUSER_ID
+
+
+def update_existing_picking_type(cr, registry):
+    """
+    This post-init-hook will update all existing issue assigning them the
+    corresponding sequence code.
+    """
+
+    obj_data = registry["ir.model.data"]
+
+    # Good Receipt
+
+    subtype_id = obj_data.get_object_reference(
+        cr, SUPERUSER_ID, "stock_operation_subtype", "good_receipt_subtype")[1]
+
+    sql = """
+    UPDATE stock_picking_type
+    SET subtype_id = %d
+    WHERE id IN (SELECT in_type_id FROM stock_warehouse);
+    """ % (subtype_id)
+
+    cr.execute(sql)
+
+    # Internal Transfer
+
+    subtype_id = obj_data.get_object_reference(
+        cr, SUPERUSER_ID, "stock_operation_subtype",
+        "internal_transfer_subtype")[1]
+
+    sql = """
+    UPDATE stock_picking_type
+    SET subtype_id = %d
+    WHERE id IN (SELECT int_type_id FROM stock_warehouse);
+    """ % (subtype_id)
+
+    cr.execute(sql)
+
+    # Delivery Order
+
+    subtype_id = obj_data.get_object_reference(
+        cr, SUPERUSER_ID, "stock_operation_subtype",
+        "delivery_order_subtype")[1]
+
+    sql = """
+    UPDATE stock_picking_type
+    SET subtype_id = %d
+    WHERE id IN (SELECT out_type_id FROM stock_warehouse);
+    """ % (subtype_id)
+
+    cr.execute(sql)
+
+    # Pick
+
+    subtype_id = obj_data.get_object_reference(
+        cr, SUPERUSER_ID, "stock_operation_subtype",
+        "pick_subtype")[1]
+
+    sql = """
+    UPDATE stock_picking_type
+    SET subtype_id = %d
+    WHERE id IN (SELECT pick_type_id FROM stock_warehouse);
+    """ % (subtype_id)
+
+    cr.execute(sql)
+
+    # Pack
+
+    subtype_id = obj_data.get_object_reference(
+        cr, SUPERUSER_ID, "stock_operation_subtype",
+        "pack_subtype")[1]
+
+    sql = """
+    UPDATE stock_picking_type
+    SET subtype_id = %d
+    WHERE id IN (SELECT pack_type_id FROM stock_warehouse);
+    """ % (subtype_id)
+
+    cr.execute(sql)
---
+++ stock_operation_subtype/models/stock_picking_subtype.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockPickingSubtype(models.Model):
+    _name = "stock.picking_subtype"
+    _description = "Stock Operation Subtype"
+
+    name = fields.Char(
+        string="Operation Subtype",
+        required=True,
+    )
+    code = fields.Char(
+        string="Code",
+    )
+    active = fields.Boolean(
+        string="Active",
+        default=True,
+    )
+    note = fields.Text(
+        string="Note",
+    )
