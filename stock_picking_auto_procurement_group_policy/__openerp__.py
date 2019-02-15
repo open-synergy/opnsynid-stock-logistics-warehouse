eec6f23c066cead68076441d014994a4159f1bf4
Andhitia Rama <andhitia.r@gmail.com>
2019-02-15 19:35:01 +0700

    [8.0.1.0.0] stock_picking_auto_procurement_group_policy

---
+++ stock_picking_auto_procurement_group_policy/__openerp__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Stock Picking's Auto Create Procurement Group Policy",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_picking_manual_procurement_group",
+    ],
+    "data": [
+        "views/stock_picking_type_views.xml",
+    ],
+}
---
+++ stock_picking_auto_procurement_group_policy/models/stock_picking.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, api
+
+
+class StockPicking(models.Model):
+    _inherit = "stock.picking"
+
+    @api.onchange("picking_type_id")
+    def onchange_create_procurement_group(self):
+        ptype = self.picking_type_id
+        self.create_procurement_group = False
+        if ptype:
+            self.create_procurement_group = ptype.create_procurement_group
