259b50475d48388a096e908bc11ceeb4200c0052
Andhitia Rama <andhitia.r@gmail.com>
2019-02-15 19:35:01 +0700

    [8.0.1.0.0] stock_picking_auto_procurement_group_policy

---
+++ stock_picking_auto_procurement_group_policy/models/stock_picking_type.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class StockPickingType(models.Model):
+    _inherit = "stock.picking.type"
+
+    create_procurement_group = fields.Boolean(
+        string="Create Procurement Group",
+    )
