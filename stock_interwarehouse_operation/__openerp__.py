93cb5c54524e41b90d4bf716cd9a6af023cd1355
mikevhe18 <viriyananda.michael@gmail.com>
2019-07-23 07:19:07 +0700

    [8.0.1.2.0][stock_interwarehouse_operatiion]

--- stock_interwarehouse_operation/__openerp__.py
+++ stock_interwarehouse_operation/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Inter-Warehouse Product Movement",
-    "version": "8.0.1.1.2",
+    "version": "8.0.1.2.0",
     "summary": "More control to inter-warehouse product movements",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
@@ -12,8 +12,10 @@
         "stock_route_transit",
         "stock_operation_type_location",
         "stock_warehouse_technical_information",
+        "stock_operation_subtype",
     ],
     "data": [
+        "data/stock_picking_subtype_data.xml",
         "views/stock_warehouse_view.xml"
     ],
     "installable": True,
