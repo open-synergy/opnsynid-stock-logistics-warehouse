66a9e2e1623ff55e82f8d1d5a50690c886480248
mikevhe18 <viriyananda.michael@gmail.com>
2019-07-31 16:28:44 +0700

    [8.0.2.1.1][stock_promotion_operation]

--- stock_promotion_operation/__openerp__.py
+++ stock_promotion_operation/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Promotion Operation",
-    "version": "8.0.2.0.0",
+    "version": "8.0.2.1.1",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
     "category": "Stock Management",
@@ -13,10 +13,12 @@
         "stock_route_transit",
         "stock_push_rule_picking_type",
         "stock_pull_rule_picking_type",
+        "stock_operation_subtype",
     ],
     "data": [
         "data/stock_location_datas.xml",
         "data/ir_property_datas.xml",
+        "data/stock_picking_subtype_data.xml",
         "views/res_partner_views.xml",
         "views/stock_warehouse_views.xml"
     ],
