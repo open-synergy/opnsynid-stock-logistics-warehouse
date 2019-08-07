b0aeacf09e7a3aaa62c4aee7bcd595b53e2d6a62
mikevhe18 <viriyananda.michael@gmail.com>
2019-08-07 10:40:35 +0700

    [8.0.1.1.5][stock_waste_operation]

--- stock_waste_operation/__openerp__.py
+++ stock_waste_operation/__openerp__.py
@@ -3,15 +3,17 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Waste Operation",
-    "version": "8.0.1.0.4",
+    "version": "8.0.1.1.5",
     "author": "OpenSynergy Indonesia",
     "website": "https://opensynergy-indonesia.com",
     "category": "Stock Management",
     "depends": [
         "stock_warehouse_technical_information",
         "stock_operation_type_location",
+        "stock_operation_subtype",
     ],
     "data": [
+        "data/stock_picking_subtype_data.xml",
         "views/stock_warehouse_view.xml"
     ],
     "installable": True,
