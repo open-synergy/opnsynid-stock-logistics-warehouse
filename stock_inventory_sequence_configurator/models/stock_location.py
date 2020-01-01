025b81d007b2758554fb649bc52d88595c73ce01
Andhitia Rama <andhitia.r@gmail.com>
2020-01-01 21:54:27 +0700

    [8.0.1.0.0] stock_inventory_sequence_configurator

---
+++ stock_inventory_sequence_configurator/models/stock_location.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockLocation(models.Model):
+    _inherit = "stock.location"
+    _name = "stock.location"
+
+    stock_inventory_sequence_id = fields.Many2one(
+        string="Sequence for Stock Inventory",
+        comodel_name="ir.sequence",
+    )
