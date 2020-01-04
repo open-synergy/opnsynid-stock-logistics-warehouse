b5b34356d7d17a7f309152b2ff1e25bf8df8cc9f
Andhitia Rama <andhitia.r@gmail.com>
2020-01-04 14:32:17 +0700

    [11.0.1.0.0] stock_inventory_by_product_set

---
+++ stock_inventory_by_product_set/__manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Inventory By Product Set",
+    "version": "11.0.1.0.0",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/stock_inventory_product_set_views.xml",
+        "views/stock_inventory_views.xml",
+    ]
+}
