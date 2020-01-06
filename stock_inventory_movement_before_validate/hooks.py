b2bfbfa41b3a1c97be1bfa3f3e8d893787a0f71c
Andhitia Rama <andhitia.r@gmail.com>
2020-01-07 05:08:28 +0700

    [11.0.1.0.0] stock_inventory_movement_before_validate

---
+++ stock_inventory_movement_before_validate/hooks.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+
+def post_init_hook(cr, pool):
+    sql1 = """
+    UPDATE  stock_inventory_line AS target
+    SET
+            manual_qty =  a.product_qty
+    FROM    stock_inventory_line AS a
+    WHERE   target.id = a.id
+    """
+    cr.execute(sql1)
