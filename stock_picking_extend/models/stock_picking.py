ee21f835d36385ca93ee8b3215955daa0cebc219
Andhitia Rama <andhitia.r@gmail.com>
2020-03-01 09:48:44 +0700

    [8.0.1.0.0] stock_picking_extend

---
+++ stock_picking_extend/models/stock_picking.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, fields
+
+
+class StockPicking(models.Model):
+    _name = "stock.picking"
+    _inherit = "stock.picking"
+
+    name = fields.Char(
+        string="# Document",
+    )
