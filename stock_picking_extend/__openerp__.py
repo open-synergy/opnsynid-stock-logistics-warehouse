d4fe26565f88a12b26fdd90e0c89c78cfc628ca9
Andhitia Rama <andhitia.r@gmail.com>
2020-03-01 09:48:44 +0700

    [8.0.1.0.0] stock_picking_extend

---
+++ stock_picking_extend/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Picking Extend",
+    "version": "8.0.1.0.0",
+    "category": "Stock",
+    "website": "https://opensynergy-indonesia.com",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/stock_picking_views.xml",
+    ],
+}
