1e931af110804cab035bcdeef7bc1cabaa1f751c
mikevhe18 <viriyananda.michael@gmail.com>
2020-12-15 14:16:56 +0700

    [8.0.1.0.0][stock_lease_operation]

---
+++ stock_lease_operation/models/res_partner.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Copyright 2020 OpenSynergy Indonesia
+# Copyright 2020 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class ResPartner(models.Model):
+    _inherit = "res.partner"
+
+    customer_lease_location_id = fields.Many2one(
+        string="Customer Lease Location",
+        comodel_name="stock.location",
+        company_dependent=True,
+    )
+    supplier_lease_location_id = fields.Many2one(
+        string="Supplier Lease Location",
+        comodel_name="stock.location",
+        company_dependent=True,
+    )
