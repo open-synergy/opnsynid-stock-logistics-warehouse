a24256a343d01f94757c53f6a84ead3214c2e71b
mikevhe18 <viriyananda.michael@gmail.com>
2021-01-13 14:07:33 +0700

    [12.0.1.0.0][stock_warehouse_technical_information]

---
+++ stock_warehouse_technical_information/__manifest__.py
@@ -0,0 +1,19 @@
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Stock Warehouse Technical Information",
+    "version": "12.0.1.0.0",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "author": "OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock",
+        ],
+    "data": [
+        "views/stock_warehouse_views.xml",
+        ],
+}
