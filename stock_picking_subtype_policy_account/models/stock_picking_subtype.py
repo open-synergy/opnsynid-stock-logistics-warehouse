d9d4f71365c034defdcd483945e11e3787d8f6fd
Andhitia Rama <andhitia.r@gmail.com>
2021-12-11 22:37:43 +0700

    [stock_picking_subtype_policy_account] 8.0.1.0.0

---
+++ stock_picking_subtype_policy_account/models/stock_picking_subtype.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockPickingSubtype(models.Model):
+    _name = "stock.picking_subtype"
+    _inherit = "stock.picking_subtype"
+
+    show_invoice_state_on_move_form = fields.Boolean(
+        string="Show Invoice State on Stock Move Form",
+        default=True,
+    )
