f62a581f3d644246bc213e3a1cf3b9a3abc7b688
Andhitia Rama <andhitia.r@gmail.com>
2021-12-11 22:37:43 +0700

    [stock_picking_subtype_policy_account] 8.0.1.0.0

---
+++ stock_picking_subtype_policy_account/__openerp__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author
+{
+    "name": "Stock Picking/Move Policy Based on Subtype - Account",
+    "version": "8.0.1.0.0",
+    "category": "Stock Management",
+    "website": "https://simetri-sinergi.id",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": [
+        "stock_picking_subtype_policy",
+    ],
+    "data": [
+        "views/stock_picking_subtype_views.xml",
+        "views/stock_move_views.xml",
+    ],
+    "demo": [
+    ],
+    "images": [
+        "static/description/banner.png",
+    ],
+}
