64dc03925494a97e5775e0898adf927f54caf225
Nurazmi <azmimr67@gmail.com>
2021-12-14 20:43:42 +0700

    [8.0.1.0.0] stock_quant_qrcode Task 8.0 T-005036

---
+++ stock_quant_qrcode/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Copyright 2021 OpenSynergy Indonesia
+# Copyright 2021 PT. Simetri Sinergi Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# pylint: disable=locally-disabled, manifest-required-author/
+
+{
+    "name": "Stock Quant QR Code",
+    "version": "8.0.1.0.0",
+    "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
+    "website": "https://simetri-sinergi.id",
+    "license": "AGPL-3",
+    "depends": [
+        "stock",
+        "base_qr_code",
+    ],
+    "data": [
+        "data/base_qr_content_policy_data.xml",
+        "views/stock_quant_views.xml",
+    ],
+    "installable": True,
+}
