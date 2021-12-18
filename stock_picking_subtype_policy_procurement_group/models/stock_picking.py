1eb04b682c5877dfcd2d033673ab55765289589f
Andhitia Rama <andhitia.r@gmail.com>
2021-12-18 19:04:15 +0700

    [stock_picking_subtype_policy_procurement_group] 8.0.1.0.0

---
+++ stock_picking_subtype_policy_procurement_group/models/stock_picking.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields, api
+
+
+class StockPicking(models.Model):
+    _name = "stock.picking"
+    _inherit = "stock.picking"
+
+    show_create_procurement_group_on_picking_form = fields.Boolean(
+        string="Show Create Procurement Group on Stock Picking Form",
+        related="picking_type_id.subtype_id.show_create_procurement_group_on_picking_form",
+        store=False,
+    )
+
+    @api.onchange(
+        "picking_type_id",
+    )
+    def onchange_procure_method(self):
+        result = False
+        if self.picking_type_id and self.picking_type_id.subtype_id:
+            result = self.picking_type_id.subtype_id.default_create_procurement_group
+        self.create_procurement_group = result
---
+++ stock_picking_subtype_policy_procurement_group/models/stock_picking_subtype.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 OpenSynergy Indonesia
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from openerp import models, fields
+
+
+class StockPickingSubtype(models.Model):
+    _name = "stock.picking_subtype"
+    _inherit = "stock.picking_subtype"
+
+    default_create_procurement_group = fields.Boolean(
+        string="Default Create Procurement Group",
+        default=False,
+    )
+    show_create_procurement_group_on_picking_form = fields.Boolean(
+        string="Show Create Procurement Group on Stock Picking Form",
+        default=True,
+    )
