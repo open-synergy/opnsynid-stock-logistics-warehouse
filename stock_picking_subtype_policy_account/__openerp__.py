112f652bc50528a2bedeffd531e1797d1596d330
Andhitia Rama <andhitia.r@gmail.com>
2021-12-18 19:03:49 +0700

    [stock_picking_subtype_policy_account] 8.0.1.1.0

--- stock_picking_subtype_policy_account/__openerp__.py
+++ stock_picking_subtype_policy_account/__openerp__.py
@@ -5,7 +5,7 @@
 # pylint: disable=locally-disabled, manifest-required-author
 {
     "name": "Stock Picking/Move Policy Based on Subtype - Account",
-    "version": "8.0.1.0.0",
+    "version": "8.0.1.1.0",
     "category": "Stock Management",
     "website": "https://simetri-sinergi.id",
     "author": "PT. Simetri Sinergi Indonesia, OpenSynergy Indonesia",
@@ -13,6 +13,7 @@
     "installable": True,
     "depends": [
         "stock_picking_subtype_policy",
+        "stock_account",
     ],
     "data": [
         "views/stock_picking_subtype_views.xml",
